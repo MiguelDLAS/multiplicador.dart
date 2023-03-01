fdb47ca3bac2068638b90e5c3dace9df548cf9e7
MiguelDLAS <mikiserrano2018@gmail.com>
2023-02-28 19:01:06 -0600

    multiplicador

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const multiplicadorApp());
+}
+
+class multiplicadorApp extends StatelessWidget {
+  const multiplicadorApp({super.key});
+
